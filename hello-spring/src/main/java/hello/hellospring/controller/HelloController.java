89c6856f15ef79ff7920d28e4727daf5be34b647
soleu <dlthf555@gmail.com>
2021-08-03 02:16:41 +0900

    add : API 구현방법 추가

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,28 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
-    
+    @GetMapping("hello-spring")
+    @ResponseBody //응답 body 부분에 값을 직접 넣어주겠다.
+    public String helloString(@RequestParam("name")String name){
+        return "hello"+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+        static class Hello {
+            private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name){
+                this.name=name;
+            }
+        }
 }
