85651d59fbab2a34f164b150d2181b7eb34a2b8b
soleu <dlthf555@gmail.com>
2021-08-03 01:57:18 +0900

    add : MVC 모델 구현방법 추가

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,7 @@ package hello.hellospring.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,4 +11,11 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String hellowMvc(@RequestParam(name="name",required = false)String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    
 }
