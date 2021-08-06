b6d334d63b7a7a03b8098d2993ec933f00863dc2
soleu <dlthf555@gmail.com>
2021-08-07 00:36:53 +0900

    add : DI 설정 - 컴포넌트 스캔방법 이용

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     //memberRepository를 생성과 동시에 내부에서 선언해줄 수 있도록 함
     private  final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository=memberRepository;
     }
