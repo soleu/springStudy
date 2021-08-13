6531200f474bbb26625d1e65b2cde94682ee6af1
soleu <dlthf555@gmail.com>
2021-08-14 02:45:53 +0900

    add : 스프링 데이터 JPA 구현

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -15,22 +15,23 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 //    private DataSource dataSource;
-        private EntityManager em;
+        private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em){
-        this.em=em;
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return  new MemberService(memberRepository());
+        return  new MemberService(memberRepository);
     }
 
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+       // return new JpaMemberRepository(em);
+        return memberRepository;
     }
 }
--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -1,2 +1,14 @@
-package hello.hellospring.repository;public interface SpringDataJpaMemberRepository {
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member,Long>,MemberRepository {
+    //interface가 interface를 받을때는 extends 라고한다.
+    //interface는 다중 상속이 가능하다
+
+    @Override
+    Optional<Member> findByName(String name);
 }
